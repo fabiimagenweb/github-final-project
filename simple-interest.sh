766fd9c78621b02534230816cb753a9de37f28b1
fabiimagenweb <coloradoriverafabianandres@gmail.com>
2025-07-12 22:13:19 -0500

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Este script calcula el interés simple
+# Autor: Upkar Lidder (IBM)
+# Autores adicionales: <tu usuario GitHub>
+
+echo "Introduce el capital:"
+read p
+echo "Introduce la tasa de interés por año:"
+read r
+echo "Introduce el período de tiempo en años:"
+read t
+
+s=`expr $p \* $t \* $r / 100`
+echo "El interés simple es:"
+echo $s
